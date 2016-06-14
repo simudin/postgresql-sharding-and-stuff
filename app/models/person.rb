5b3076a230b5b0f74099dff2b07c7936926793ae
Hasim Sofaruddin <hasim@41studio.com>
2016-06-14 17:08:00 +0700

    partiotion

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,9 @@
 class Person < ActiveRecord::Base
 	has_many :clones
+
+	def partition_id
+    self.id % 32
+  end
+
+  has_many :images, ->(u) { where( partition_id: u.partition_id ) }
 end
--- db/migrate/20160614085424_create_images.rb
+++ db/migrate/20160614085424_create_images.rb
@@ -1,5 +1,5 @@
 class CreateImages < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :images do |t|
       t.belongs_to :person, index: true, foreign_key: true
       t.integer :partition_id
@@ -10,47 +10,55 @@ class CreateImages < ActiveRecord::Migration
 
     partitions = 32
 
-	partitions.times do |i|
-	  execute "CREATE TABLE images_p#{i} (
-	      CHECK ( partition_id = #{i} )
-	      ) INHERITS (images)"
-	end
-	tbl = 'images'
-	execute "CREATE OR REPLACE FUNCTION #{tbl}_insert_trigger()
-               RETURNS TRIGGER AS
-               $$
-               DECLARE
-                  _tablename text;
-               BEGIN
-                   _tablename := '#{tbl}_p' || NEW.\"partition_id\";
+		partitions.times do |i|
+		  execute "CREATE TABLE images_p#{i} (
+		      CHECK ( partition_id = #{i} )
+		      ) INHERITS (images)"
+		end
+		tbl = 'images'
+		execute "CREATE OR REPLACE FUNCTION #{tbl}_insert_trigger()
+	               RETURNS TRIGGER AS
+	               $$
+	               DECLARE
+	                  _tablename text;
+	               BEGIN
+	                   _tablename := '#{tbl}_p' || NEW.\"partition_id\";
 
-                   EXECUTE 'INSERT INTO ' || quote_ident(_tablename) || ' SELECT ($1).*'
-                   USING NEW;
-                   RETURN NEW;
-               END;
-               $$
-               LANGUAGE plpgsql;"
+	                   EXECUTE 'INSERT INTO ' || quote_ident(_tablename) || ' SELECT ($1).*'
+	                   USING NEW;
+	                   RETURN NEW;
+	               END;
+	               $$
+	               LANGUAGE plpgsql;"
 
-      execute "CREATE TRIGGER insert_#{tbl}_trigger
-               BEFORE INSERT ON #{tbl}
-               FOR EACH ROW EXECUTE PROCEDURE #{tbl}_insert_trigger();"
+    execute "CREATE TRIGGER insert_#{tbl}_trigger
+             BEFORE INSERT ON #{tbl}
+             FOR EACH ROW EXECUTE PROCEDURE #{tbl}_insert_trigger();"
 
-      execute "CREATE OR REPLACE FUNCTION #{tbl}_delete_master()
-               RETURNS trigger
-               AS $$
-               DECLARE
-                   r #{tbl}%rowtype;
-               BEGIN
-                   DELETE FROM ONLY #{tbl} where id = NEW.id returning * into r;
-                   RETURN r;
-               END;
-               $$
-               LANGUAGE plpgsql;"
+    execute "CREATE OR REPLACE FUNCTION #{tbl}_delete_master()
+             RETURNS trigger
+             AS $$
+             DECLARE
+                 r #{tbl}%rowtype;
+             BEGIN
+                 DELETE FROM ONLY #{tbl} where id = NEW.id returning * into r;
+                 RETURN r;
+             END;
+             $$
+             LANGUAGE plpgsql;"
 
-      execute "create trigger after_insert_#{tbl}_trigger
-               after insert on #{tbl}
-               for each row
-                   execute procedure #{tbl}_delete_master();
-               end;"
+    execute "create trigger after_insert_#{tbl}_trigger
+             after insert on #{tbl}
+             for each row
+                 execute procedure #{tbl}_delete_master();
+             end;"
+  end
+
+  def self.down
+  	partitions = 32
+		partitions.times do |i|
+		  execute "DROP TABLE images_p#{i}"
+		end
+		drop_table :images
   end
 end
